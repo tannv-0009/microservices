f16090aac4e405e00e6365f70893c8f5f5afeb5f
Nguyen Van Tan <nguyen.van.tan@sun-asterisk.com>
2020-08-05 16:41:38 +0700

    init

--- configurations/src/main/java/com/tannv/configurations/ConfigurationsApplication.java
+++ configurations/src/main/java/com/tannv/configurations/ConfigurationsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+
 public class ConfigurationsApplication {
 
 	public static void main(String[] args) {
